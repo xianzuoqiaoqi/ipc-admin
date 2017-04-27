129cb8018944aacb1979d2e0f9276d793f6a38f0
zzz <865077695@qq.com>
2017-04-27 18:50:08 +0800

    no message

---
+++ res/tpl/routers/addUserController.js
@@ -0,0 +1,1 @@
+define(function(require){var app=require("app");require("myService"),require("load"),require("loadCss").loadCss("res/css/addUser.css"),app.controller("addUserController",function($scope,myService){console.log("addUser"),$scope.sub=function(){console.log("正在提交。。。 。..")}})});
