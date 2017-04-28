11b457e3cdcc5c8ce26e016ea8b1b4fbefdd957c
zzz <865077695@qq.com>
2017-04-28 11:54:54 +0800

    no message

--- dev/script/index.js
+++ dev/script/index.js
@@ -2,19 +2,23 @@
  * Created by zzq on 2017/3/17.
  * 启动angular
  */
-define(["require", "layer"], function (require,layer)
+define(["require", "layer"], function (require, layer)
 {
     layer.config({
-        path:'res/lib/layer/'
+        path: 'res/lib/layer/'
     });
-    var startLoading = layer.load(1,{shade:[0.5,'#000']});   //开始执行动画
-    require(["angular","routes","myDirective"],function(angular){
+    var startLoading = layer.load(1, {
+        shade: [0.5, '#000'],
+        area:['37px','37px']
+    });
+    require(["angular", "routes", "myDirective"], function (angular)
+    {
         require(['domReady!'], function (document)
         {     //domReady依赖的!前缀来强制require()回调函数在执行之前等待DOM Ready。
             /*手工启动angular*/
             window.loading.finish(function ()
             {
-                angular.bootstrap(document,['app']);
+                angular.bootstrap(document, ['app']);
                 // layer.close(startLoading);//结束动画
             });
         });
