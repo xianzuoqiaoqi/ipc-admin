a17e907301a576c0743cc2aa9865343674a00fee
zzz <865077695@qq.com>
2017-07-31 18:51:20 +0800

    按照新文档重新修改

--- dev/script/myService.js
+++ dev/script/myService.js
@@ -3,29 +3,37 @@
  * 公用方法在factory定义。
  * 依赖app
  */
-define(['require', 'app','layer'], function (require, app,layer)
-{
-    
+define(['require', 'app', 'layer'], function (require, app, layer) {
+
     //服务，存放全局数据
-    app.factory('myService', function ($http)
-    {
+    app.factory('myService', function ($http) {
         var service = {};
         //ajax获取json数据
         service.url = "https://easy-mock.com/mock/5902c5887a878d73716ded63/ipc-admin/";
-        service.getJson = function (url,requestData,method)
-        {
+        service.getJson = function (url, requestData, method) {
             var index = layer.load(1, {
-                shade: [0.5,'#000'] //0.1透明度的白色背景
+                shade: [0.5, '#000'] //0.1透明度的白色背景
             });
             return $http({
-                method: method||'GET',
-                url:service.url+url,
-                params:requestData || '',
-                data:requestData || null
-            }).success(function(){
+                method: method || 'GET',
+                url: service.url + url,
+                params: requestData || '',
+                data: requestData || null
+            }).success(function () {
                 layer.close(index);
             });
         };
         return service;
+    }).factory('storage', function ($window) {
+        var set = function (key, value) {
+            $window.localStorage[key] = value || 15;
+        }
+        var get = function (key) {
+            return $window.localStorage[key] || 15
+        }
+        return {
+            set: set,
+            get: get
+        }
     });
 })
--- dev/tpl/routers/userListController.js
+++ dev/tpl/routers/userListController.js
@@ -1,21 +1,23 @@
 /**
  * Created by zzq on 2017/4/10.
  */
-define(function(require){
+define(function (require) {
     //加载所需模块
     var app = require('app');
     require('myService');
     loadCss = require('loadCss');
     loadCss.loadCss('res/css/userList.css');//引入css
-    
-    app.controller('userListController',function($scope,myService){
+
+    app.controller('userListController', function ($scope, myService, storage) {
+        console.log(storage)
+        // storage.set('pageSize', 10)
         //日期插件配置及操作
         //初始化查询条件
         $scope.dateOption = {
             startTime: '',
             endTime: '',
         };
-    
+
         //时间选择器配置
         $scope.startDateOptions = {
             formatYear: 'yy',
@@ -27,36 +29,43 @@ define(function(require){
             maxDate: new Date(),
             startingDay: 1
         };
-    
+
         //当有一个时间改变时，重置另一个的时间范围
-        $scope.updateMin = function(){
+        $scope.updateMin = function () {
             $scope.endDateOptions.minDate = $scope.dateOption.startTime;
             //当开始时间改变时，设置pageInfo.startTime
-            $scope.pageInfo.startTime = isNaN(new Date($scope.dateOption.startTime).getTime())?'':new Date($scope.dateOption.startTime).getTime();
+            $scope.pageInfo.startTime = isNaN(new Date($scope.dateOption.startTime).getTime()) ? '' : new Date($scope.dateOption.startTime).getTime();
         };
-        $scope.updateMax = function(){
+        $scope.updateMax = function () {
             $scope.startDateOptions.maxDate = $scope.dateOption.endTime;
             //当结束时间改变时，设置pageInfo.endTime
-            $scope.pageInfo.endTime = isNaN(new Date($scope.dateOption.endTime).getTime())?'':new Date($scope.dateOption.endTime).getTime();
+            $scope.pageInfo.endTime = isNaN(new Date($scope.dateOption.endTime).getTime()) ? '' : new Date($scope.dateOption.endTime).getTime();
         };
-    
+
         //时间面板展开隐藏设置
-        $scope.startOpen = function() {
+        $scope.startOpen = function () {
             $scope.startPopupOpened = true;
         };
-        $scope.endOpen = function() {
+        $scope.endOpen = function () {
             $scope.endPopupOpened = true;
         };
         $scope.startPopupOpened = false;
         $scope.endPopupOpened = false;
 
-        
+        // select下拉框选取设备状态
+        $scope.devStatus = [
+            {statusName: '未激活', statusId: '0'},
+            {statusName: '激活', statusId: '1'},
+            {statusName: '失效', statusId: '-1'},
+            {statusName: '其他', statusId: '2'}
+        ]
+
         //定义请求某页数据的函数，传入请求地址和请求参数
-        var showUserList = function(url,requestData,method){
-            
-            myService.getJson(url,requestData,method)
+        var showUserList = function (url, requestData, method) {
+
+            myService.getJson(url, requestData, method)
             //请求成功时的回调函数，将获取的数据进行处理
-                .success(function(data){
+                .success(function (data) {
                     $scope.userInfoList = data.userInfoList;//将获取到的列表数据存入scope
                     $scope.totalCount = data.totalCount;//将总条目数存入scope
                     $scope.maxSize = 5;//设置显示的页码数量
@@ -65,37 +74,53 @@ define(function(require){
         };
         //初始化向后台请求数据时携带的参数
         $scope.pageInfo = {
-            loginType:'',//账号类型
-            loginName:'',//账号名
-            p2pId:'',//
-            startTime:'',
-            endTime:'',
-            startPage:1,      //索要的页码
-            pageSize:2     //每页条数
+            loginType: '',//账号类型
+            loginName: '',//账号名
+            p2pId: '',//
+            startTime: '',
+            endTime: '',
+            startPage: 1,    //索要的页码
+            pageSize: storage.get('pageSize'),     //初始化从localstorage中获取每页条数。
+            devStatus: ''    // 设备状态
         };
-        
-        
-        
+
+
         //路由加载完成后默认请求第一页
         var url = 'kinzo-cms/user/accounts'//联测请求后台数据
-        
+
         //页面初始化显示
-        showUserList(url,$scope.pageInfo);//发起请求，联测
-        
+        showUserList(url, $scope.pageInfo);//发起请求，联测
+
         //点击页码时将索要的页码存入ajax请求参数内
-        $scope.pageChange = function(){
+        $scope.pageChange = function () {
             $scope.pageInfo.startPage = $scope.startPage;
             //携带新参数重新请求
-            showUserList(url,$scope.pageInfo);//携带新参数重新请求，联测
+            showUserList(url, $scope.pageInfo);//携带新参数重新请求，联测
         };
-        
+
         //条件查询
-        $scope.goSearch = function(){
+        $scope.goSearch = function () {
             $scope.pageInfo.startPage = 1;//页码初始化为1
             //开始查询
             console.log($scope.pageInfo)
             console.log($scope.pageInfo.startTime)
-            showUserList(url,$scope.pageInfo);//发起请求，联测
+            showUserList(url, $scope.pageInfo);//发起请求，联测
+        };
+
+        // 自定义每页条数
+        $scope.pageSizeChange = function () {
+            $scope.pageInfo.pageSize = $scope.pageSize; // 存入pageInfo
+            storage.set('pageSize', $scope.pageSize);   // 存入localstorage
+            // 携带新参数重新发起请求
+            showUserList(url, $scope.pageInfo)
         }
+
+        // 修改设备激活状态
+        $scope.changeStatus = function (index) {
+            console.log(index)
+            // 在此处提交新状态，成功后在回调函数内执行下面代码
+            $scope.userInfoList[index].accountStatus = $scope.userInfoList[index].accountStatus == 'active' ? 'inActive' : 'active'
+        }
+
     })
 })
